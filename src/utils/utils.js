839f0071f16c635c820ce4382081281fcda4e9fc
eduardvorsin <eduardvorsin2019@yandex.kz>
2023-01-13 13:41:22 +0600

    added getWeatherStatusByCode function

--- src/utils/utils.js
+++ src/utils/utils.js
@@ -13,3 +13,29 @@ export const isCorrectHeadingLevel = (level) => {
 
   return true;
 };
+
+export const getWeatherStatusByCode = (code) => {
+  if (typeof code !== 'number') {
+    throw new Error('code parameter should be a number type');
+  }
+
+  let status = '';
+
+  if (code === 0 || code === 1) {
+    status = 'ясно';
+  } else if (code === 2 || code === 3) {
+    status = 'пасмурно';
+  } else if (code === 45) {
+    status = 'туман';
+  } else if ((code >= 48 && code <= 67) || (code >= 80 && code <= 86)) {
+    status = 'дождь';
+  } else if (code >= 71 && code <= 77) {
+    status = 'снег';
+  } else if (code >= 95 && code <= 99) {
+    status = 'гроза';
+  } else {
+    throw new Error('recived unknown weather code');
+  }
+
+  return status;
+};
